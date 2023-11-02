ece442f03346f543ae3adcbd6fe3e28f601de8a0
GabeALopez <109094200+GabeALopez@users.noreply.github.com>
2023-11-02 16:02:52 -0400

    Finished upload function

---
+++ client-basic.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author : Ayesha S. Dina
+
+#Template used from author
+
+import os
+import socket
+
+IP = "localhost"
+PORT = 4450
+ADDR = (IP, PORT)
+SIZE = 1024  ## byte .. buffer size
+FORMAT = "utf-8"
+SERVER_DATA_PATH = "server_data"
+
+
+def main():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client.connect(ADDR)
+    while True:  ### multiple communications
+        data = client.recv(SIZE).decode(FORMAT)
+        cmd, msg = data.split("@")
+        if cmd == "OK":
+            print(f"{msg}")
+        elif cmd == "DISCONNECTED":
+            print(f"{msg}")
+            break
+
+        data = input("> ")
+        data = data.split(" ")
+        cmd = data[0]
+
+        if cmd == "TASK":
+            client.send(cmd.encode(FORMAT))
+
+        elif cmd == "LOGOUT":
+            client.send(cmd.encode(FORMAT))
+            break
+        elif cmd == "UPLOAD":
+            file_path = data[1]
+            upload_file(client, file_path)
+
+    print("Disconnected from the server.")
+    client.close()  ## close the connection
+
+
+def upload_file(client, file_path):
+    global file_name
+    try:
+        file_name = os.path.basename(file_path)
+        client.send(f"UPLOAD@{file_name}".encode(FORMAT))
+
+        with open(file_name, "rb") as file:
+            file_data = file.read(SIZE)
+            while file_data:
+                client.send(file_data)
+                file_data = file.read(SIZE)
+            print(f"File '{file_name}' uploaded successfully.")
+    except FileNotFoundError:
+        print(f"File '{file_name}' not found")
+    except Exception as e:
+        print(f"Error while sending file: {str(e)}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ server-basic.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Author : Ayesha S. Dina
+
+import os
+import socket
+import threading
+
+IP = "localhost"  ### gethostname()
+PORT = 4450
+ADDR = (IP, PORT)
+SIZE = 1024
+FORMAT = "utf-8"
+SERVER_PATH = "server"
+
+
+### to handle the clients
+def handle_client(conn, addr):
+    print(f"NEW CONNECTION: {addr} connected.")
+    conn.send("OK@Welcome to the server".encode(FORMAT))
+
+    while True:
+        data = conn.recv(SIZE).decode(FORMAT)
+        data = data.split("@")
+        cmd = data[0]
+
+        send_data = "OK@"
+
+        if cmd == "LOGOUT":
+            break
+
+        elif cmd == "TASK":
+            send_data += "LOGOUT from the server.\n"
+            conn.send(send_data.encode(FORMAT))
+
+        elif cmd == "UPLOAD":
+            file_name = data[1]
+            receive_file(conn, file_name)
+
+    print(f"{addr} disconnected")
+    conn.close()
+
+
+def receive_file(conn, file_name):
+    try:
+        directory_path = "Server_Path"
+        file_path = os.path.join(directory_path, file_name)
+
+        os.makedirs(directory_path, exist_ok=True)
+
+        with open(file_path, "wb") as file:
+            file_data = conn.recv(SIZE)
+            while file_data:
+                file.write(file_data)
+                file_data = conn.recv(SIZE)
+            print(f"File '{file_name}' received successfully.")
+    except Exception as e:
+        print(f"Error while received file: {str(e)}")
+
+
+def main():
+    print("Starting the server")
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  ## used IPV4 and TCP connection
+    server.bind(ADDR)  # bind the address
+    server.listen()  ## start listening
+    print(f"server is listening on {IP}: {PORT}")
+    while True:
+        conn, addr = server.accept()  ### accept a connection from a client
+        thread = threading.Thread(target=handle_client, args=(conn, addr))  ## assigning a thread for each client
+        thread.start()
+
+
+if __name__ == "__main__":
+    main()
